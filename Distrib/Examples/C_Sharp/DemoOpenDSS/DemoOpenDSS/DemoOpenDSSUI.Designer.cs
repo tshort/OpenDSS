292b623944e64bf2c7fcb565660d192141feb08d
rdugan <rdugan@d8739450-1e93-4ef4-a0af-c327d92816ff>
2013-10-14 20:13:18 +0000

    Csharp examples
    git-svn-id: svn://svn.code.sf.net/p/electricdss/code/trunk@945 d8739450-1e93-4ef4-a0af-c327d92816ff

---
+++ Distrib/Examples/C_Sharp/DemoOpenDSS/DemoOpenDSS/DemoOpenDSSUI.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace DemoOpenDSS
+{
+   partial class DemoOpenDSSUI
+   {
+      /// <summary>
+      /// Required designer variable.
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary>
+      /// Clean up any resources being used.
+      /// </summary>
+      /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+      protected override void Dispose( bool disposing )
+      {
+         if ( disposing && ( components != null ) )
+         {
+            components.Dispose();
+         }
+         base.Dispose( disposing );
+      }
+
+      #region Windows Form Designer generated code
+
+      /// <summary>
+      /// Required method for Designer support - do not modify
+      /// the contents of this method with the code editor.
+      /// </summary>
+      private void InitializeComponent()
+      {
+         this.startDSS = new System.Windows.Forms.Button();
+         this.textBox1 = new System.Windows.Forms.TextBox();
+         this.label1 = new System.Windows.Forms.Label();
+         this.loadCircuit = new System.Windows.Forms.Button();
+         this.solveCircuit = new System.Windows.Forms.Button();
+         this.loadSeqVoltages = new System.Windows.Forms.Button();
+         this.loadMagAngle = new System.Windows.Forms.Button();
+         this.loadSeqCurrents = new System.Windows.Forms.Button();
+         this.loadLoadPowers = new System.Windows.Forms.Button();
+         this.loadVoltages = new System.Windows.Forms.Button();
+         this.SuspendLayout();
+         // 
+         // startDSS
+         // 
+         this.startDSS.Font = new System.Drawing.Font( "Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ( ( byte ) ( 0 ) ) );
+         this.startDSS.Location = new System.Drawing.Point( 100, 22 );
+         this.startDSS.Name = "startDSS";
+         this.startDSS.Size = new System.Drawing.Size( 112, 38 );
+         this.startDSS.TabIndex = 0;
+         this.startDSS.Text = "Start DSS";
+         this.startDSS.UseVisualStyleBackColor = true;
+         this.startDSS.Click += new System.EventHandler( this.starDSS_Click );
+         // 
+         // textBox1
+         // 
+         this.textBox1.Location = new System.Drawing.Point( 120, 109 );
+         this.textBox1.Name = "textBox1";
+         this.textBox1.Size = new System.Drawing.Size( 410, 20 );
+         this.textBox1.TabIndex = 1;
+         this.textBox1.Text = "C:\\Program Files\\OpenDSS\\IEEETestCases\\123Bus\\IEEE123Master.DSS";
+         // 
+         // label1
+         // 
+         this.label1.AutoSize = true;
+         this.label1.Font = new System.Drawing.Font( "Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ( ( byte ) ( 0 ) ) );
+         this.label1.Location = new System.Drawing.Point( 13, 112 );
+         this.label1.Name = "label1";
+         this.label1.Size = new System.Drawing.Size( 101, 13 );
+         this.label1.TabIndex = 2;
+         this.label1.Text = "File Path Name :";
+         // 
+         // loadCircuit
+         // 
+         this.loadCircuit.Font = new System.Drawing.Font( "Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ( ( byte ) ( 0 ) ) );
+         this.loadCircuit.Location = new System.Drawing.Point( 100, 179 );
+         this.loadCircuit.Name = "loadCircuit";
+         this.loadCircuit.Size = new System.Drawing.Size( 225, 38 );
+         this.loadCircuit.TabIndex = 3;
+         this.loadCircuit.Text = "Compile the Circuit File";
+         this.loadCircuit.UseVisualStyleBackColor = true;
+         this.loadCircuit.Click += new System.EventHandler( this.loadCircuit_Click );
+         // 
+         // solveCircuit
+         // 
+         this.solveCircuit.Font = new System.Drawing.Font( "Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ( ( byte ) ( 0 ) ) );
+         this.solveCircuit.Location = new System.Drawing.Point( 100, 247 );
+         this.solveCircuit.Name = "solveCircuit";
+         this.solveCircuit.Size = new System.Drawing.Size( 225, 38 );
+         this.solveCircuit.TabIndex = 4;
+         this.solveCircuit.Text = "Solve the Circuit";
+         this.solveCircuit.UseVisualStyleBackColor = true;
+         this.solveCircuit.Click += new System.EventHandler( this.solveCircuit_Click );
+         // 
+         // loadSeqVoltages
+         // 
+         this.loadSeqVoltages.Font = new System.Drawing.Font( "Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ( ( byte ) ( 0 ) ) );
+         this.loadSeqVoltages.Location = new System.Drawing.Point( 100, 383 );
+         this.loadSeqVoltages.Name = "loadSeqVoltages";
+         this.loadSeqVoltages.Size = new System.Drawing.Size( 225, 38 );
+         this.loadSeqVoltages.TabIndex = 5;
+         this.loadSeqVoltages.Text = "Load the Seq Voltages";
+         this.loadSeqVoltages.UseVisualStyleBackColor = true;
+         this.loadSeqVoltages.Click += new System.EventHandler( this.loadSeqVoltages_Click );
+         // 
+         // loadMagAngle
+         // 
+         this.loadMagAngle.Font = new System.Drawing.Font( "Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ( ( byte ) ( 0 ) ) );
+         this.loadMagAngle.Location = new System.Drawing.Point( 100, 451 );
+         this.loadMagAngle.Name = "loadMagAngle";
+         this.loadMagAngle.Size = new System.Drawing.Size( 225, 38 );
+         this.loadMagAngle.TabIndex = 6;
+         this.loadMagAngle.Text = "Load the Mag/Angle Sheet";
+         this.loadMagAngle.UseVisualStyleBackColor = true;
+         this.loadMagAngle.Click += new System.EventHandler( this.loadMagAngle_Click );
+         // 
+         // loadSeqCurrents
+         // 
+         this.loadSeqCurrents.Font = new System.Drawing.Font( "Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ( ( byte ) ( 0 ) ) );
+         this.loadSeqCurrents.Location = new System.Drawing.Point( 100, 519 );
+         this.loadSeqCurrents.Name = "loadSeqCurrents";
+         this.loadSeqCurrents.Size = new System.Drawing.Size( 225, 38 );
+         this.loadSeqCurrents.TabIndex = 7;
+         this.loadSeqCurrents.Text = "Load the Seq Currents";
+         this.loadSeqCurrents.UseVisualStyleBackColor = true;
+         this.loadSeqCurrents.Click += new System.EventHandler( this.loadSeqCurrents_Click );
+         // 
+         // loadLoadPowers
+         // 
+         this.loadLoadPowers.Font = new System.Drawing.Font( "Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ( ( byte ) ( 0 ) ) );
+         this.loadLoadPowers.Location = new System.Drawing.Point( 100, 587 );
+         this.loadLoadPowers.Name = "loadLoadPowers";
+         this.loadLoadPowers.Size = new System.Drawing.Size( 225, 38 );
+         this.loadLoadPowers.TabIndex = 8;
+         this.loadLoadPowers.Text = "Load the Load Powers";
+         this.loadLoadPowers.UseVisualStyleBackColor = true;
+         this.loadLoadPowers.Click += new System.EventHandler( this.loadLoadPowers_Click );
+         // 
+         // loadVoltages
+         // 
+         this.loadVoltages.Font = new System.Drawing.Font( "Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ( ( byte ) ( 0 ) ) );
+         this.loadVoltages.Location = new System.Drawing.Point( 100, 315 );
+         this.loadVoltages.Name = "loadVoltages";
+         this.loadVoltages.Size = new System.Drawing.Size( 225, 38 );
+         this.loadVoltages.TabIndex = 9;
+         this.loadVoltages.Text = "Load the Voltages";
+         this.loadVoltages.UseVisualStyleBackColor = true;
+         this.loadVoltages.Click += new System.EventHandler( this.loadVoltages_Click );
+         // 
+         // DemoOpenDSSUI
+         // 
+         this.AutoScaleDimensions = new System.Drawing.SizeF( 6F, 13F );
+         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+         this.ClientSize = new System.Drawing.Size( 600, 732 );
+         this.Controls.Add( this.loadVoltages );
+         this.Controls.Add( this.loadLoadPowers );
+         this.Controls.Add( this.loadSeqCurrents );
+         this.Controls.Add( this.loadMagAngle );
+         this.Controls.Add( this.loadSeqVoltages );
+         this.Controls.Add( this.solveCircuit );
+         this.Controls.Add( this.loadCircuit );
+         this.Controls.Add( this.label1 );
+         this.Controls.Add( this.textBox1 );
+         this.Controls.Add( this.startDSS );
+         this.Name = "DemoOpenDSSUI";
+         this.Text = "OpenDSS Demo";
+         this.FormClosed += new System.Windows.Forms.FormClosedEventHandler( this.DemoOpenDSSUI_FormClosed );
+         this.ResumeLayout( false );
+         this.PerformLayout();
+
+      }
+
+      #endregion
+
+      private System.Windows.Forms.Button startDSS;
+      private System.Windows.Forms.TextBox textBox1;
+      private System.Windows.Forms.Label label1;
+      private System.Windows.Forms.Button loadCircuit;
+      private System.Windows.Forms.Button solveCircuit;
+      private System.Windows.Forms.Button loadSeqVoltages;
+      private System.Windows.Forms.Button loadMagAngle;
+      private System.Windows.Forms.Button loadSeqCurrents;
+      private System.Windows.Forms.Button loadLoadPowers;
+      private System.Windows.Forms.Button loadVoltages;
+   }
+}
+
---
+++ Distrib/Examples/C_Sharp/DemoOpenDSS/DemoOpenDSS/DemoOpenDSSUI.cs
@@ -0,0 +1,306 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Reflection;
+using System.Diagnostics;
+using OpenDSSengine;
+using Microsoft.Office.Interop.Excel;
+
+namespace DemoOpenDSS
+{
+   public partial class DemoOpenDSSUI : Form
+   {
+      public DSS DSSobj;
+      public Text DSSText;
+      public Circuit DSSCircuit;
+      public Solution DSSSolution;
+      public CtrlQueue DSSControlQueue;
+
+      public static _Application application = new Microsoft.Office.Interop.Excel.Application();
+      public static string pathApp = Path.GetDirectoryName( Assembly.GetExecutingAssembly().Location );
+
+      public static string filePath = pathApp + @"\results.xlsx";
+      public _Workbook workbook = application.Workbooks.Open( filePath, Type.Missing, Type.Missing, Type.Missing,
+                                             Type.Missing, Type.Missing, Type.Missing, Type.Missing,
+                                             Type.Missing, Type.Missing, Type.Missing, Type.Missing,
+                                             Type.Missing, Type.Missing, Type.Missing );
+           
+      
+      public DemoOpenDSSUI()
+      {
+         InitializeComponent();
+      }
+
+
+      private void starDSS_Click( object sender, EventArgs e )
+      {
+         DSSobj = new DSS();
+         if ( !( DSSobj.Start( 0 ) ) )
+         {
+            MessageBox.Show( "DSS failed to start" );
+         }
+         else
+         {
+            MessageBox.Show( "DSS started sucessfully" );
+            DSSText = DSSobj.Text;
+         }
+      }
+
+
+      private void loadCircuit_Click( object sender, EventArgs e )
+      {
+         string circuitName = textBox1.Text;
+                  
+         DSSText.Command = "clear";
+         DSSText.Command = "compile " + "(" + circuitName +")";
+         DSSCircuit = DSSobj.ActiveCircuit;
+         DSSSolution = DSSCircuit.Solution;
+         DSSControlQueue = DSSCircuit.CtrlQueue;
+
+         MessageBox.Show( "Circuit Loaded" );
+
+      }
+
+      private void solveCircuit_Click( object sender, EventArgs e )
+      {
+         DSSSolution.Solve();
+         if ( DSSSolution.Converged )
+            MessageBox.Show( "Solution Converged" );
+         else
+            MessageBox.Show( "Solution did not converge" );
+      }
+
+      private void loadSeqVoltages_Click( object sender, EventArgs e )
+      {
+         Bus DSSBus;
+         int iRow, iCol, i, j;
+         dynamic V, cpxV;
+       
+         _Worksheet worksheet = (Worksheet) workbook.Worksheets["SeqVoltSheet"];
+
+         iRow = 2;
+
+         for ( i = 1 ; i <= DSSCircuit.NumBuses ; i++ )
+         {
+            DSSBus = DSSCircuit.Buses[i];
+            worksheet.Cells[iRow, 1].Value = DSSCircuit.ActiveBus.Name;
+
+            V = DSSBus.SeqVoltages;
+            cpxV = DSSBus.CplxSeqVoltages;
+
+            iCol = 2;
+            for ( j = 0 ; j < V.Length ; j++ ) 
+            {
+               worksheet.Cells[iRow, iCol].Value = V[j];
+               iCol = iCol + 1;
+            }
+
+            iCol = 6;
+            for ( j = 0 ; j < cpxV.Length ; j++ )
+            {
+               worksheet.Cells[iRow, iCol].Value = cpxV[j];
+               iCol = iCol + 1;
+            }
+
+
+            iRow = iRow + 1;
+         
+         }
+
+         workbook.Save();
+     }
+
+      private void loadVoltages_Click( object sender, EventArgs e )
+      {
+         Bus DSSBus;
+         int iRow, iCol, i, j;
+         dynamic V;
+
+         _Worksheet worksheet = ( Worksheet ) workbook.Worksheets["VoltSheet"];
+
+         iRow = 2;
+
+         for ( i = 1 ; i <= DSSCircuit.NumBuses ; i++ )
+         {
+            DSSBus = DSSCircuit.Buses[i];
+            worksheet.Cells[iRow, 1].Value = DSSCircuit.ActiveBus.Name;
+
+            V = DSSBus.puVoltages;
+
+            iCol = 2;
+            for ( j = 0 ; j < V.Length ; j++ )
+            {
+               worksheet.Cells[iRow, iCol].Value = V[j];
+               iCol = iCol + 1;
+            }
+
+            iRow = iRow + 1;
+
+         }
+
+         workbook.Save();
+
+      }
+
+      private void loadMagAngle_Click( object sender, EventArgs e )
+      {
+         Bus DSSBus;
+         int iRow, iCol, i, j;
+         dynamic V;
+
+         _Worksheet worksheet = ( Worksheet ) workbook.Worksheets["MagSheet"];
+
+         iRow = 2;
+
+         for ( i = 1 ; i <= DSSCircuit.NumBuses ; i++ )
+         {
+            DSSBus = DSSCircuit.Buses[i];
+            worksheet.Cells[iRow, 1].Value = DSSCircuit.ActiveBus.Name;
+
+            V = DSSBus.puVoltages;
+
+            iCol = 2;
+            for ( j = 0 ; j < V.Length ; j=j+2 )
+            {
+               worksheet.Cells[iRow, iCol].Value = Math.Sqrt(V[j]*V[j]+V[j+1]*V[j+1]);
+               worksheet.Cells[iRow, iCol + 1].Value = angleC(V[j],V[j+1]);
+               iCol = iCol + 2;
+            }
+
+            iRow = iRow + 1;
+
+         }
+
+         workbook.Save();
+
+      }
+
+      public static double angleC( double Re, double Im )
+      {
+         double angle;
+         if ( Re != 0 )
+         {
+            angle = Math.Atan( Im / Re ) * 180 / Math.PI;
+            if ( Re < 0 )
+            {
+               angle = angle + 180;
+               return angle;
+            }
+            else 
+               return angle;
+         }
+         else 
+         {
+            if ( Im > 0 )
+               return 90;
+            else if ( Im < 0 )
+               return -90;
+            else
+               return 0;
+            }      
+      }
+
+      private void loadSeqCurrents_Click( object sender, EventArgs e )
+      {
+         CktElement DSSElement;
+         int iRow, iCol, i, j;
+         dynamic SeqCurr, cpxSeqValues;
+
+         _Worksheet worksheet = ( Worksheet ) workbook.Worksheets["SeqCurrSheet"];
+
+
+         DSSElement = DSSCircuit.ActiveCktElement;
+
+         iRow = 3;
+         i = DSSCircuit.FirstPDElement();
+
+         while (i>0)
+         {
+            if ( DSSElement.NumPhases == 3 )
+            {
+               worksheet.Cells[iRow, 1].Value = DSSElement.Name;
+
+               SeqCurr = DSSElement.SeqCurrents;
+               cpxSeqValues = DSSElement.CplxSeqCurrents;
+
+               iCol = 2;
+               for ( j = 0 ; j < SeqCurr.Length ; j++ )
+               {
+                  worksheet.Cells[iRow, iCol].Value = SeqCurr[j];
+                  iCol = iCol + 1;
+               }
+
+               iCol = 9;
+               for ( j = 0 ; j < cpxSeqValues.Length ; j++ )
+               {
+                  worksheet.Cells[iRow, iCol].Value = cpxSeqValues[j];
+                  iCol = iCol + 1;
+               }
+               iRow = iRow + 1;
+            }
+            i = DSSCircuit.NextPDElement();
+         }
+
+         workbook.Save();
+
+      }
+
+      private void loadLoadPowers_Click( object sender, EventArgs e )
+      {
+         CktElement DSSElement;
+         int iRow, iCol, i, j;
+         dynamic PCPowers;
+         double SumkW, Sumkvar;
+
+         _Worksheet worksheet = ( Worksheet ) workbook.Worksheets["LoadPowers"];
+
+         DSSElement = DSSCircuit.ActiveCktElement;
+
+         iRow = 2;
+         i = DSSCircuit.Loads.First;
+
+         while ( i > 0 )
+         {
+               worksheet.Cells[iRow, 1].Value = DSSElement.Name;
+
+               PCPowers = DSSElement.Powers;
+
+               iCol = 2;
+               SumkW = 0;
+               Sumkvar = 0;
+
+               for ( j = 0 ; j < PCPowers.Length ; j=j+2 )
+               {
+                  SumkW = SumkW + PCPowers[j];
+                  Sumkvar = Sumkvar + PCPowers[j+1];
+               }
+
+               worksheet.Cells[iRow, iCol].Value = SumkW;
+               worksheet.Cells[iRow, iCol+1].Value = Sumkvar;
+
+               iRow = iRow + 1;
+            i = DSSCircuit.Loads.Next;
+         }
+
+         workbook.Save();
+      }
+
+      private void DemoOpenDSSUI_FormClosed( object sender, FormClosedEventArgs e )
+      {
+         if ( workbook != null )
+            workbook.Close( false, Type.Missing, Type.Missing );
+         workbook = null;
+         if ( application != null )
+            application.Quit();
+         application = null;
+      }
+
+
+   }
+}
