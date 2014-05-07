39a396b6528d6c6b16c8a0b9bca3e68545f4d2e6
rdugan <rdugan@d8739450-1e93-4ef4-a0af-c327d92816ff>
2014-05-07 03:31:23 +0000

    Update to Loadshape to implemment PQCSVFile command to load in P, Q Pairs
    git-svn-id: svn://svn.code.sf.net/p/electricdss/code/trunk@979 d8739450-1e93-4ef4-a0af-c327d92816ff

--- Source/General/LoadShape.pas
+++ Source/General/LoadShape.pas
@@ -56,6 +56,7 @@ TYPE
        Procedure Set_Code(const Value:String);  // sets the  active LoadShape
 
        Procedure DoCSVFile(Const FileName:String);
+       Procedure Do2ColCSVFile(Const FileName:String);  // for P and Q pairs
        Procedure DoSngFile(Const FileName:String);
        Procedure DoDblFile(Const FileName:String);
      Protected
@@ -147,7 +148,7 @@ implementation
 
 USES  ParserDel,  DSSClassDefs, DSSGlobals, Sysutils,  MathUtil, Utilities, Classes, TOPExport, Math, PointerList;
 
-Const NumPropsThisClass = 18;
+Const NumPropsThisClass = 20;
 
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 constructor TLoadShape.Create;  // Creates superstructure for all Line objects
@@ -187,11 +188,11 @@ Begin
      PropertyName[4] := 'hour';     // vextor of hour values
      PropertyName[5] := 'mean';     // set the mean (otherwise computed)
      PropertyName[6] := 'stddev';   // set the std dev (otherwise computed)
-     PropertyName[7] := 'csvfile';   // Switch input to a csvfile
+     PropertyName[7] := 'csvfile';  // Switch input to a csvfile
      PropertyName[8] := 'sngfile';  // switch input to a binary file of singles
-     PropertyName[9] := 'dblfile';   // switch input to a binary file of singles
-     PropertyName[10] := 'action'; // actions  Normalize
-     PropertyName[11] := 'qmult'; // Q multiplier
+     PropertyName[9] := 'dblfile';  // switch input to a binary file of singles
+     PropertyName[10] := 'action';  // actions  Normalize
+     PropertyName[11] := 'qmult';   // Q multiplier
      PropertyName[12] := 'UseActual'; // Flag to signify to use actual value
      PropertyName[13] := 'Pmax'; // MaxP value
      PropertyName[14] := 'Qmax'; // MaxQ
@@ -199,6 +200,8 @@ Begin
      PropertyName[16] := 'minterval'; // Interval in minutes
      PropertyName[17] := 'Pbase'; // for normalization, use peak if 0
      PropertyName[18] := 'Qbase'; // for normalization, use peak if 0
+     PropertyName[19] := 'Pmult'; // synonym for Mult
+     PropertyName[20] := 'PQCSVFile'; // Redirect to a file with p, q pairs
 
      // define Property help values
 
@@ -213,9 +216,10 @@ Begin
                         'mult = (dblfile=filename)  !for packed file of doubles'+CRLF+
                         'mult = (sngfile=filename)  !for packed file of singles '+CRLF+
                         'mult = (file=MyCSVFile.CSV, col=3, header=yes)  !for multicolumn CSV files '+CRLF+CRLF+
-                        'Note: this property will reset Npts if the  number of values in the files are fewer.';     // vextor of hour values
+                        'Note: this property will reset Npts if the  number of values in the files are fewer.' + CRLF+CRLF+
+                        'Same as Pmult';     // vector of power multiplier values
      PropertyHelp[4] := 'Array of hour values. Only necessary to define for variable interval data.'+
-                    ' If the data are fixed interval, do not use this property. ' +
+                    ' If the data are FIXED interval, DO NOT USE THIS PROPERTY. ' +
                     'You can also use the syntax: '+CRLF+
                         'hour = (file=filename)     !for text file one value per line'+CRLF+
                         'hour = (dblfile=filename)  !for packed file of doubles'+CRLF+
@@ -227,7 +231,7 @@ Begin
                      'value is needed.  However, you may set it to another value independently.'+
                      'Is overwritten if you subsequently read in a curve' + CRLF + CRLF +
                      'Used for Monte Carlo load simulations.';   // set the std dev (otherwise computed)
-     PropertyHelp[7] := 'Switch input of active power load curve data to a csv file '+
+     PropertyHelp[7] := 'Switch input of active power load curve data to a CSV text file '+
                         'containing (hour, mult) points, or simply (mult) values for fixed time interval data, one per line. ' +
                         'NOTE: This action may reset the number of points to a lower value.';   // Switch input to a csvfile
      PropertyHelp[8] := 'Switch input of active power load curve data to a binary file of singles '+
@@ -245,8 +249,9 @@ Begin
      PropertyHelp[11] := 'Array of multiplier values for reactive power (Q).  You can also use the syntax: '+CRLF+
                         'qmult = (file=filename)     !for text file one value per line'+CRLF+
                         'qmult = (dblfile=filename)  !for packed file of doubles'+CRLF+
-                        'qmult = (sngfile=filename)  !for packed file of singles ';     // vector of qmultiplier values
-     PropertyHelp[12] := '{Yes | No* | True | False*} If true, signals to Load, Generator, Vsource, or other objects to ' +
+                        'qmult = (sngfile=filename)  !for packed file of singles ' + CRLF +     // vector of qmultiplier values
+                        'qmult = (file=MyCSVFile.CSV, col=4, header=yes)  !for multicolumn CSV files ';
+     PropertyHelp[12] := '{Yes | No* | True | False*} If true, signifies to Load, Generator, Vsource, or other objects to ' +
                          'use the return value as the actual kW, kvar, kV, or other value rather than a multiplier. ' +
                          'Nominally for AMI Load data but may be used for other functions.';
      PropertyHelp[13] := 'kW value at the time of max power. Is automatically set upon reading in a loadshape. '+
@@ -257,6 +262,9 @@ Begin
      PropertyHelp[16] := 'Specify fixed interval in MINUTES. Alternate way to specify Interval property.';
      PropertyHelp[17] := 'Base P value for normalization. Default is zero, meaning the peak will be used.';
      PropertyHelp[18] := 'Base Q value for normalization. Default is zero, meaning the peak will be used.';
+     PropertyHelp[19] := 'Synonym for "mult".';
+     PropertyHelp[20] := 'Switch input to a CSV text file containing (active, reactive) power (P, Q) multiplier pairs, one per row. '+CRLF+
+                         'If the interval=0, there should be 3 items on each line: (hour, Pmult, Qmult)';
 
      ActiveProperty := NumPropsThisClass;
      inherited DefineProperties;  // Add defs of inherited properties to bottom of list
@@ -334,6 +342,13 @@ BEGIN
            16: Interval := Parser.DblValue / 60.0;  // Convert minutes to hr
            17: BaseP := Parser.DblValue;
            18: BaseQ := Parser.DblValue;
+           19: BEGIN   // same as mult
+                 ReAllocmem(PMultipliers, Sizeof(PMultipliers^[1])*NumPoints);
+                 // Allow possible Resetting (to a lower value) of num points when specifying multipliers not Hours
+                 NumPoints := InterpretDblArray(Param, NumPoints, PMultipliers);   // Parser.ParseAsVector(Npts, Multipliers);
+               END;
+           20: Do2ColCSVFile(Param);
+
          ELSE
            // Inherited parameters
              ClassEdit( ActiveLoadShapeObj, ParamPointer - NumPropsThisClass)
@@ -450,6 +465,62 @@ BEGIN
 END;
 
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+procedure TLoadShape.Do2ColCSVFile(const FileName: String);
+{
+   Process 2-column CSV file (3-col if time expected)
+}
+VAR
+    F:Textfile;
+    i:Integer;
+    s:String;
+
+BEGIN
+    TRY
+       AssignFile(F,FileName);
+       Reset(F);
+    EXCEPT
+       DoSimpleMsg('Error Opening File: "' + FileName, 613);
+       CloseFile(F);
+       Exit;
+    END;
+
+    TRY
+
+       WITH ActiveLoadShapeObj DO
+       BEGIN
+         // Allocate both P and Q multipliers
+         ReAllocmem(PMultipliers, Sizeof(PMultipliers^[1])*NumPoints);
+         ReAllocmem(QMultipliers, Sizeof(QMultipliers^[1])*NumPoints);
+         IF Interval=0.0 THEN ReAllocmem(Hours, Sizeof(Hours^[1])*NumPoints);
+         i := 0;
+         WHILE (NOT EOF(F)) AND (i<FNumPoints) DO
+         BEGIN
+            Inc(i);
+            Readln(F, s); // read entire line  and parse with AuxParser
+            {AuxParser allows commas or white space}
+            With AuxParser Do Begin
+                CmdString := s;
+                IF Interval=0.0 THEN Begin
+                   NextParam; Hours^[i] := DblValue;
+                End;
+                NextParam; PMultipliers^[i] := DblValue;  // first parm
+                NextParam; QMultipliers^[i] := DblValue;  // second parm
+            End;
+          END;
+         CloseFile(F);
+         If i<>FNumPoints Then NumPoints := i;
+        END;
+
+    EXCEPT
+       On E:Exception Do Begin
+         DoSimpleMsg('Error Processing CSV File: "' + FileName + '. ' + E.Message , 614);
+         CloseFile(F);
+         Exit;
+       End;
+    END;
+
+end;
+
 Procedure TLoadShape.DoCSVFile(Const FileName:String);
 
 VAR
@@ -872,6 +943,7 @@ begin
           16: Result := Format('%.8g', [Interval * 60.0 ]);
           17: Result := Format('%.8g', [BaseP ]);
           18: Result := Format('%.8g', [BaseQ ]);
+          19: Result := GetDSSArray_Real( FNumPoints, PMultipliers);
         ELSE
            Result := Inherited GetPropertyValue(index);
         END;
@@ -897,6 +969,10 @@ begin
      PropertyValue[14] := '0';
      PropertyValue[15] := '3600';   // seconds
      PropertyValue[16] := '60';     // minutes
+     PropertyValue[17] := '0';
+     PropertyValue[18] := '0';
+     PropertyValue[19] := '';   // same as 3
+     PropertyValue[20] := '';  // switch input to csv file of P, Q pairs
 
 
 
