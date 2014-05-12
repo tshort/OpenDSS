658a08804643e31a0298ce5918a6c94559a046d4
rdugan <rdugan@d8739450-1e93-4ef4-a0af-c327d92816ff>
2014-05-12 19:02:14 +0000

    git-svn-id: svn://svn.code.sf.net/p/electricdss/code/trunk@982 d8739450-1e93-4ef4-a0af-c327d92816ff

---
+++ Distrib/Examples/C_Sharp/WorkshopExample/WindowsFormsApplication2/WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenDSSengine;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public DSS DSSObj;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DSSObj = new DSS();
+            if (DSSObj.Start(0))
+            {
+              MessageBox.Show("OpenDSS Started OK"); 
+            };
+
+
+        }
+    }
+}
