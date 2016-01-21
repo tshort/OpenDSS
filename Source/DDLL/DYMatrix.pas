970fdf63e4b3c823101cf357e926859ce05c8cd1
davismont <davismont@d8739450-1e93-4ef4-a0af-c327d92816ff>
2016-01-21 02:06:24 +0000

    New interface added to the "Alternative interfaces" unit.
    git-svn-id: svn://svn.code.sf.net/p/electricdss/code/trunk@1285 d8739450-1e93-4ef4-a0af-c327d92816ff

--- Source/DDLL/DYMatrix.pas
+++ Source/DDLL/DYMatrix.pas
@@ -2,21 +2,32 @@ unit DYMatrix;
 
 interface
 
-Uses Arraydef, UComplex;
-
+Uses Arraydef, UComplex, Solution;
 
 Function  InitAndGetYparams(var hY, nBus, nNZ:LongWord): Longword; StdCall;
 Procedure GetCompressedYMatrix(hY, nBus, nNz:Longword; Var ColPtr, RowIdx:pIntegerArray; Var cVals:pComplexArray); StdCall;
+//01202016
+procedure ZeroInjCurr; Stdcall;
+procedure GetSourceInjCurrents; Stdcall;
+procedure GetPCInjCurr; Stdcall;
+function SystemYChanged(mode, arg: longint): longint; Stdcall;
+procedure BuildYMatrixD(BuildOps, AllocateVI: longint); Stdcall;
+function UseAuxCurrents(mode, arg: longint): longint; Stdcall;
+procedure AddInAuxCurrents(SType: integer); Stdcall;
+procedure getIpointer(var IvectorPtr: pComplexArray);Stdcall;
+procedure getVpointer(var VvectorPtr: pComplexArray);Stdcall;
+function SolveSystem(var NodeV:pNodeVarray): integer; stdcall;
+
 
 implementation
 
-Uses DSSGlobals,Solution, Ymatrix;
+Uses DSSGlobals, Ymatrix;
 
 Var {Global variables in this Module}
    Yhandle, NumNZ, NumBuses : LongWord;
    YColumns,
    YRows   : pIntegerArray;
-   YValues : pComplexArray;
+   YValues, IVector, VVector : pComplexArray;
 
 
 Function InitAndGetYparams(var hY, nBus, nNZ:LongWord): Longword; StdCall;
@@ -68,6 +79,77 @@ Begin
      cVals  := YValues;
 End;
 
+procedure ZeroInjCurr; Stdcall;
+Begin
+     IF ActiveCircuit <> Nil THEN ActiveCircuit.Solution.ZeroInjCurr;
+end;
+
+procedure GetSourceInjCurrents; Stdcall;
+Begin
+     IF ActiveCircuit <> Nil THEN ActiveCircuit.Solution.GetSourceInjCurrents;
+end;
+
+procedure GetPCInjCurr; Stdcall;
+Begin
+     IF ActiveCircuit <> Nil THEN ActiveCircuit.Solution.GetPCInjCurr;
+end;
+
+function SystemYChanged(mode, arg: longint): longint; Stdcall;
+begin
+    Result:=0;
+    case mode of
+        0: if ActiveCircuit.Solution.SystemYChanged then  Result:=1;  // Read
+        1: begin                                                      // Write
+           if arg=1 then ActiveCircuit.Solution.SystemYChanged:= TRUE
+           else ActiveCircuit.Solution.SystemYChanged:= FALSE;
+        end;
+    end;
+end;
+
+procedure BuildYMatrixD(BuildOps, AllocateVI: longint); Stdcall;
+var
+  AllocateV: boolean;
+begin
+   AllocateV:=FALSE;
+   if AllocateVI=1 then AllocateV:=TRUE;
+   BuildYMatrix(BuildOps,AllocateV);
+end;
+
+function UseAuxCurrents(mode, arg: longint): longint; Stdcall;
+begin
+    Result:=0;
+    case mode of
+        0: if ActiveCircuit.Solution.UseAuxCurrents then  Result:=1;  // Read
+        1: begin                                                      // Write
+           if arg=1 then ActiveCircuit.Solution.UseAuxCurrents:= TRUE
+           else ActiveCircuit.Solution.UseAuxCurrents:= FALSE;
+        end;
+    end;
+end;
+
+procedure AddInAuxCurrents(SType: integer); Stdcall;
+begin
+    ActiveCircuit.Solution.AddInAuxCurrents(SType);
+end;
+
+procedure getIpointer(var IvectorPtr: pComplexArray);Stdcall;
+begin
+     ReAllocmem(IVector,sizeof(IVector^[1])*NumNZ);
+     IVectorPtr:=IVector;
+end;
+
+procedure getVpointer(var VvectorPtr: pComplexArray);Stdcall;
+begin
+     ReAllocmem(VVector,sizeof(VVector^[1])*NumNZ);
+     VVectorPtr:=VVector;
+end;
+
+function SolveSystem(var NodeV:pNodeVarray): integer; stdcall;
+begin
+  Result:=ActiveCircuit.Solution.SolveSystem(NodeV);
+end;
+
+//---------------------------------------------------------------------------------
 
 initialization
 
