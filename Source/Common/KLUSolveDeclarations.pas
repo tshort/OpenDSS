a30a744f7959775b898848999baf869bc7b91d2c
temcdrm <temcdrm@d8739450-1e93-4ef4-a0af-c327d92816ff>
2016-04-10 19:24:35 +0000

    prepping for 7.6.5, building in Delphi XE7 KLUSolve is back to stdcall
    git-svn-id: svn://svn.code.sf.net/p/electricdss/code/trunk@1445 d8739450-1e93-4ef4-a0af-c327d92816ff

--- Source/Common/KLUSolveDeclarations.pas
+++ Source/Common/KLUSolveDeclarations.pas
@@ -3,69 +3,70 @@
 
 // in general, KLU arrays are 0-based
 // function calls return 0 to indicate failure, 1 for success
+// stdcall instead of cdecl for 32-bit Windows; this maps to fastcall on 64-bit Windows
 
 // returns the non-zero handle of a new sparse matrix, if successful
 // must call DeleteSparseSet on the valid handle when finished
-FUNCTION NewSparseSet(nBus:LongWord):NativeUInt;cdecl;external 'KLUSolve.DLL';
+FUNCTION NewSparseSet(nBus:LongWord):NativeUInt;stdcall;external 'KLUSolve.DLL';
 // return 1 for success, 0 for invalid handle
-FUNCTION DeleteSparseSet(id:NativeUInt):LongWord;cdecl;external 'KLUSolve.DLL';
+FUNCTION DeleteSparseSet(id:NativeUInt):LongWord;stdcall;external 'KLUSolve.DLL';
 
 // return 1 for success, 2 for singular, 0 for invalid handle
 // factors matrix if needed
-FUNCTION SolveSparseSet(id:NativeUInt; x,b:pComplexArray):LongWord;cdecl;external 'KLUSolve.DLL';
+FUNCTION SolveSparseSet(id:NativeUInt; x,b:pComplexArray):LongWord;stdcall;external 'KLUSolve.DLL';
 
 // return 1 for success, 0 for invalid handle
-FUNCTION ZeroSparseSet(id:NativeUInt):LongWord;cdecl;external 'KLUSolve.DLL';
+FUNCTION ZeroSparseSet(id:NativeUInt):LongWord;stdcall;external 'KLUSolve.DLL';
 
 // return 1 for success, 2 for singular, 0 for invalid handle
 // FactorSparseMatrix does no extra work if the factoring was done previously
-FUNCTION FactorSparseMatrix(id:NativeUInt):LongWord;cdecl;external 'KLUSolve.DLL';
+FUNCTION FactorSparseMatrix(id:NativeUInt):LongWord;stdcall;external 'KLUSolve.DLL';
 
 // These "Get" functions for matrix information all return 1 for success, 0 for invalid handle
 // Res is the matrix order (number of nodes)
-FUNCTION GetSize(id:NativeUInt; Res: pLongWord):LongWord;cdecl;external 'KLUSolve.DLL';
+FUNCTION GetSize(id:NativeUInt; Res: pLongWord):LongWord;stdcall;external 'KLUSolve.DLL';
 
 // the following function results are not known prior to factoring
 // Res is the number of floating point operations to factor
-FUNCTION GetFlops(id:NativeUInt; Res: pDouble):LongWord;cdecl;external 'KLUSolve.DLL';
+FUNCTION GetFlops(id:NativeUInt; Res: pDouble):LongWord;stdcall;external 'KLUSolve.DLL';
 // Res is number of non-zero entries in the original matrix
-FUNCTION GetNNZ(id:NativeUInt; Res: pLongWord):LongWord;cdecl;external 'KLUSolve.DLL';
+FUNCTION GetNNZ(id:NativeUInt; Res: pLongWord):LongWord;stdcall;external 'KLUSolve.DLL';
 // Res is the number of non-zero entries in factored matrix
-FUNCTION GetSparseNNZ(id:NativeUInt; Res: pLongWord):LongWord;cdecl;external 'KLUSolve.DLL';
+FUNCTION GetSparseNNZ(id:NativeUInt; Res: pLongWord):LongWord;stdcall;external 'KLUSolve.DLL';
 // Res is a column number corresponding to a singularity, or 0 if not singular
-FUNCTION GetSingularCol(id:NativeUInt; Res: pLongWord):LongWord;cdecl;external 'KLUSolve.DLL';
+FUNCTION GetSingularCol(id:NativeUInt; Res: pLongWord):LongWord;stdcall;external 'KLUSolve.DLL';
 // Res is the pivot element growth factor
-FUNCTION GetRGrowth(id:NativeUInt; Res: pDouble):LongWord;cdecl;external 'KLUSolve.DLL';
+FUNCTION GetRGrowth(id:NativeUInt; Res: pDouble):LongWord;stdcall;external 'KLUSolve.DLL';
 // Res is aquick estimate of the reciprocal of condition number
-FUNCTION GetRCond(id:NativeUInt; Res: pDouble):LongWord;cdecl;external 'KLUSolve.DLL';
+FUNCTION GetRCond(id:NativeUInt; Res: pDouble):LongWord;stdcall;external 'KLUSolve.DLL';
 // Res is a more accurate estimate of condition number
-FUNCTION GetCondEst(id:NativeUInt; Res: pDouble):LongWord;cdecl;external 'KLUSolve.DLL';
+FUNCTION GetCondEst(id:NativeUInt; Res: pDouble):LongWord;stdcall;external 'KLUSolve.DLL';
 
 // return 1 for success, 0 for invalid handle or a node number out of range
-FUNCTION AddPrimitiveMatrix(id:NativeUInt; nOrder:LongWord; Nodes: pLongWord; Mat: pComplex):LongWord;cdecl;external 'KLUSolve.DLL';
+FUNCTION AddPrimitiveMatrix(id:NativeUInt; nOrder:LongWord; Nodes: pLongWord; Mat: pComplex):LongWord;stdcall;external 'KLUSolve.DLL';
 
 // Action = 0 (close), 1 (rewrite) or 2 (append)
-FUNCTION SetLogFile(Path: pChar; Action:LongWord):LongWord;cdecl;external 'KLUSolve.DLL';
+FUNCTION SetLogFile(Path: pChar; Action:LongWord):LongWord;stdcall;external 'KLUSolve.DLL';
 
 // fill sparse matrix in compressed column form
 // return 1 for success, 0 for invalid handle, 2 for invalid array sizes
 // pColP must be of length nColP == nBus + 1
 // pRowIdx and pMat of length nNZ, which
 //    must be at least the value returned by GetNNZ
-FUNCTION GetCompressedMatrix(id:NativeUInt; nColP, nNZ:LongWord; pColP, pRowIdx: pLongWord; Mat: pComplex):LongWord;cdecl;external 'KLUSolve.DLL';
+FUNCTION GetCompressedMatrix(id:NativeUInt; nColP, nNZ:LongWord; pColP, pRowIdx: pLongWord; Mat: pComplex):LongWord;stdcall;external 'KLUSolve.DLL';
 
 // fill sparse matrix in triplet form
 // return 1 for success, 0 for invalid handle, 2 for invalid array sizes
 // pRows, pCols, and Mat must all be of length nNZ
-FUNCTION GetTripletMatrix(id:NativeUInt; nNZ:LongWord; pRows, pCols: pLongWord; Mat: pComplex):LongWord;cdecl;external 'KLUSolve.DLL';
+FUNCTION GetTripletMatrix(id:NativeUInt; nNZ:LongWord; pRows, pCols: pLongWord; Mat: pComplex):LongWord;stdcall;external 'KLUSolve.DLL';
 
 // returns number of islands >= 1 by graph traversal
 // pNodes contains the island number for each node
-FUNCTION FindIslands(id:NativeUInt; nOrder:LongWord; pNodes: pLongWord):LongWord;cdecl;external 'KLUSolve.DLL';
+FUNCTION FindIslands(id:NativeUInt; nOrder:LongWord; pNodes: pLongWord):LongWord;stdcall;external 'KLUSolve.DLL';
 
 // AddMatrixElement is deprecated, use AddPrimitiveMatrix instead
-FUNCTION AddMatrixElement(id:NativeUInt; i,j:LongWord; Value:pComplex):LongWord;cdecl;external 'KLUSolve.DLL';
+FUNCTION AddMatrixElement(id:NativeUInt; i,j:LongWord; Value:pComplex):LongWord;stdcall;external 'KLUSolve.DLL';
 
 // GetMatrixElement is deprecated, use GetCompressedMatrix or GetTripletMatrix
-FUNCTION GetMatrixElement(id:NativeUInt; i,j:LongWord; Value:pComplex):LongWord;cdecl;external 'KLUSolve.DLL';
+FUNCTION GetMatrixElement(id:NativeUInt; i,j:LongWord; Value:pComplex):LongWord;stdcall;external 'KLUSolve.DLL';
 
