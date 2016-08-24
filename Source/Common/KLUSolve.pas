cfe0fdc7c71e724697c59eba6aa9f9e6d697a5f2
temcdrm <temcdrm@d8739450-1e93-4ef4-a0af-c327d92816ff>
2016-08-24 04:44:47 +0000

    fix for 64-bit crashes
    git-svn-id: svn://svn.code.sf.net/p/electricdss/code/trunk@1575 d8739450-1e93-4ef4-a0af-c327d92816ff

--- Source/Common/KLUSolve.pas
+++ Source/Common/KLUSolve.pas
@@ -13,43 +13,43 @@ const
 
 // returns the non-zero handle of a new sparse matrix, if successful
 // must call DeleteSparseSet on the valid handle when finished
-FUNCTION NewSparseSet(nBus:LongWord):LongWord;stdcall;external KLULib;
+FUNCTION NewSparseSet(nBus:LongWord):NativeUInt;stdcall;external KLULib;
 // return 1 for success, 0 for invalid handle
-FUNCTION DeleteSparseSet(id:LongWord):LongWord;stdcall;external KLULib;
+FUNCTION DeleteSparseSet(id:NativeUInt):LongWord;stdcall;external KLULib;
 
 // return 1 for success, 2 for singular, 0 for invalid handle
 // factors matrix if needed
-FUNCTION SolveSparseSet(id:LongWord; x,b:pComplexArray):LongWord;stdcall;external KLULib;
+FUNCTION SolveSparseSet(id:NativeUInt; x,b:pComplexArray):LongWord;stdcall;external KLULib;
 
 // return 1 for success, 0 for invalid handle
-FUNCTION ZeroSparseSet(id:LongWord):LongWord;stdcall;external KLULib;
+FUNCTION ZeroSparseSet(id:NativeUInt):LongWord;stdcall;external KLULib;
 
 // return 1 for success, 2 for singular, 0 for invalid handle
 // FactorSparseMatrix does no extra work if the factoring was done previously
-FUNCTION FactorSparseMatrix(id:LongWord):LongWord;stdcall;external KLULib;
+FUNCTION FactorSparseMatrix(id:NativeUInt):LongWord;stdcall;external KLULib;
 
 // These "Get" functions for matrix information all return 1 for success, 0 for invalid handle
 // Res is the matrix order (number of nodes)
-FUNCTION GetSize(id:LongWord; Res: pLongWord):LongWord;stdcall;external KLULib;
+FUNCTION GetSize(id:NativeUInt; Res: pLongWord):LongWord;stdcall;external KLULib;
 
 // the following function results are not known prior to factoring
 // Res is the number of floating point operations to factor
-FUNCTION GetFlops(id:LongWord; Res: pDouble):LongWord;stdcall;external KLULib;
+FUNCTION GetFlops(id:NativeUInt; Res: pDouble):LongWord;stdcall;external KLULib;
 // Res is number of non-zero entries in the original matrix
-FUNCTION GetNNZ(id:LongWord; Res: pLongWord):LongWord;stdcall;external KLULib;
+FUNCTION GetNNZ(id:NativeUInt; Res: pLongWord):LongWord;stdcall;external KLULib;
 // Res is the number of non-zero entries in factored matrix
-FUNCTION GetSparseNNZ(id:LongWord; Res: pLongWord):LongWord;stdcall;external KLULib;
+FUNCTION GetSparseNNZ(id:NativeUInt; Res: pLongWord):LongWord;stdcall;external KLULib;
 // Res is a column number corresponding to a singularity, or 0 if not singular
-FUNCTION GetSingularCol(id:LongWord; Res: pLongWord):LongWord;stdcall;external KLULib;
+FUNCTION GetSingularCol(id:NativeUInt; Res: pLongWord):LongWord;stdcall;external KLULib;
 // Res is the pivot element growth factor
-FUNCTION GetRGrowth(id:LongWord; Res: pDouble):LongWord;stdcall;external KLULib;
+FUNCTION GetRGrowth(id:NativeUInt; Res: pDouble):LongWord;stdcall;external KLULib;
 // Res is aquick estimate of the reciprocal of condition number
-FUNCTION GetRCond(id:LongWord; Res: pDouble):LongWord;stdcall;external KLULib;
+FUNCTION GetRCond(id:NativeUInt; Res: pDouble):LongWord;stdcall;external KLULib;
 // Res is a more accurate estimate of condition number
-FUNCTION GetCondEst(id:LongWord; Res: pDouble):LongWord;stdcall;external KLULib;
+FUNCTION GetCondEst(id:NativeUInt; Res: pDouble):LongWord;stdcall;external KLULib;
 
 // return 1 for success, 0 for invalid handle or a node number out of range
-FUNCTION AddPrimitiveMatrix(id, nOrder:LongWord; Nodes: pLongWord; Mat: pComplex):LongWord;stdcall;external KLULib;
+FUNCTION AddPrimitiveMatrix(id:NativeUInt; nOrder:LongWord; Nodes: pLongWord; Mat: pComplex):LongWord;stdcall;external KLULib;
 
 // Action = 0 (close), 1 (rewrite) or 2 (append)
 FUNCTION SetLogFile(Path: pChar; Action:LongWord):LongWord;stdcall;external KLULib;
@@ -59,22 +59,22 @@ FUNCTION SetLogFile(Path: pChar; Action:LongWord):LongWord;stdcall;external KLUL
 // pColP must be of length nColP == nBus + 1
 // pRowIdx and pMat of length nNZ, which
 //    must be at least the value returned by GetNNZ
-FUNCTION GetCompressedMatrix(id, nColP, nNZ:LongWord; pColP, pRowIdx: pLongWord; Mat: pComplex):LongWord;stdcall;external KLULib;
+FUNCTION GetCompressedMatrix(id:NativeUInt; nColP, nNZ:LongWord; pColP, pRowIdx: pLongWord; Mat: pComplex):LongWord;stdcall;external KLULib;
 
 // fill sparse matrix in triplet form
 // return 1 for success, 0 for invalid handle, 2 for invalid array sizes
 // pRows, pCols, and Mat must all be of length nNZ
-FUNCTION GetTripletMatrix(id, nNZ:LongWord; pRows, pCols: pLongWord; Mat: pComplex):LongWord;stdcall;external KLULib;
+FUNCTION GetTripletMatrix(id:NativeUInt; nNZ:LongWord; pRows, pCols: pLongWord; Mat: pComplex):LongWord;stdcall;external KLULib;
 
 // returns number of islands >= 1 by graph traversal
 // pNodes contains the island number for each node
-FUNCTION FindIslands(id, nOrder:LongWord; pNodes: pLongWord):LongWord;stdcall;external KLULib;
+FUNCTION FindIslands(id:NativeUInt; nOrder:LongWord; pNodes: pLongWord):LongWord;stdcall;external KLULib;
 
 // AddMatrixElement is deprecated, use AddPrimitiveMatrix instead
-FUNCTION AddMatrixElement(id:LongWord; i,j:LongWord; Value:pComplex):LongWord;stdcall;external KLULib;
+FUNCTION AddMatrixElement(id:NativeUInt; i,j:LongWord; Value:pComplex):LongWord;stdcall;external KLULib;
 
 // GetMatrixElement is deprecated, use GetCompressedMatrix or GetTripletMatrix
-FUNCTION GetMatrixElement(id:LongWord; i,j:LongWord; Value:pComplex):LongWord;stdcall;external KLULib;
+FUNCTION GetMatrixElement(id:NativeUInt; i,j:LongWord; Value:pComplex):LongWord;stdcall;external KLULib;
 
 implementation
 
