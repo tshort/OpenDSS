1055551c51e685ae220e4049a07a1792784aea88
temcdrm <temcdrm@d8739450-1e93-4ef4-a0af-c327d92816ff>
2016-10-13 20:34:12 +0000

    update for CIM 16 and Apache Jena on Windows
    git-svn-id: svn://svn.code.sf.net/p/electricdss/code/trunk@1621 d8739450-1e93-4ef4-a0af-c327d92816ff

--- Source/cdpsm_import/go.bat
+++ Source/cdpsm_import/go.bat
@@ -1,5 +1,26 @@
-call compile.bat
-rem call do_import.bat -p=c -e=i -f=50 -v=1 -s=1000 -q=y aigue_edf_v1-1-c15.xml aigue_edf
-rem call do_import.bat -p=c -e=i -f=50 -v=1 -s=1000 -q=y c10_edf_v1-1-c15.xml c10_edf
-call do_import.bat -p=c -e=u -f=60 -v=0.001 -s=0.001 -q=n circ_combined_50204_2011_03_29_1752_50.xml circ_combined
+set JENA_HOME=c:\apache-jena-3.1.0
+set CLASSPATH=.;c:/apache-jena-3.1.0/lib/*
+
+javac CDPSM_to_DSS.java
+
+java CDPSM_to_DSS -p=c -e=u -f=60 -v=0.001 -s=0.001 -q=y IEEE13Nodeckt_CDPSM_Combined.XML ieee13
+java CDPSM_to_DSS -p=c -e=u -f=60 -v=0.001 -s=0.001 -q=y IEEE13NodecktAssets_CDPSM_Combined.XML ieee13assets
+java CDPSM_to_DSS -p=c -e=u -f=60 -v=0.001 -s=0.001 -q=y IEEE8500u_CDPSM_Combined.XML ieee8500u
+
+rem java CDPSM_to_DSS -p=c -e=i -f=60 -v=1 -s=1 -q=y 4BusYYbal_CDPSM_Combined.XML ieee4yy
+rem java CDPSM_to_DSS -p=c -e=i -f=60 -v=1 -s=1 -q=y 4busDYBal_CDPSM_Combined.XML ieee4dy
+rem java CDPSM_to_DSS -p=c -e=i -f=60 -v=1 -s=1 -q=y 4busOYODBal_CDPSM_Combined.XML ieee4oyod
+rem java CDPSM_to_DSS -p=c -e=i -f=60 -v=1 -s=1 -q=y ieee4OYOD-ubal_CDPSM_Combined.XML ieee4oyodu
+rem java CDPSM_to_DSS -p=c -e=i -f=60 -v=1 -s=1 -q=y 4busYDBal_CDPSM_Combined.XML ieee4yd
+rem java CDPSM_to_DSS -p=c -e=i -f=60 -v=1 -s=1 -q=y DGProtFdr_CDPSM_Combined.XML ieeeDG
+rem java CDPSM_to_DSS -p=c -e=i -f=60 -v=1 -s=1 -q=y IEEE_30_CDPSM_Combined.XML ieee30
+rem java CDPSM_to_DSS -p=c -e=i -f=60 -v=1 -s=1 -q=y NEV_CDPSM_Combined.XML ieeeNEV
+rem java CDPSM_to_DSS -p=c -e=i -f=60 -v=1 -s=1 -q=y ieee123_CDPSM_Combined.XML ieee123
+rem java CDPSM_to_DSS -p=c -e=i -f=60 -v=1 -s=1 -q=y ieee34-2_CDPSM_Combined.XML ieee34
+rem java CDPSM_to_DSS -p=c -e=i -f=60 -v=1 -s=1 -q=y ieee37_CDPSM_Combined.XML ieee37
+rem java CDPSM_to_DSS -p=c -e=i -f=60 -v=1 -s=1 -q=y IEEE8500_CDPSM_Combined.XML ieee8500
+rem java CDPSM_to_DSS -p=c -e=i -f=60 -v=1 -s=1 -q=y ckt5_CDPSM_Combined.XML epri5
+rem java CDPSM_to_DSS -p=c -e=i -f=60 -v=1 -s=1 -q=y ckt7_CDPSM_Combined.XML epri7
+rem java CDPSM_to_DSS -p=c -e=i -f=60 -v=1 -s=1 -q=y ckt24_CDPSM_Combined.XML epri24
+
 
