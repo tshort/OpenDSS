6871d8f7541cd726f30c94981fc97f13e95bd126
rdugan <rdugan@d8739450-1e93-4ef4-a0af-c327d92816ff>
2016-10-28 18:29:58 +0000

    Fixed bug in IntervalHrs implementation
    git-svn-id: svn://svn.code.sf.net/p/electricdss/code/trunk@1646 d8739450-1e93-4ef4-a0af-c327d92816ff

--- Source/DLL/ImplSolution.pas
+++ Source/DLL/ImplSolution.pas
@@ -225,7 +225,7 @@ procedure TSolution.Set_StepSize(Value: Double);
 begin
      If ActiveCircuit <> Nil Then Begin
          ActiveCircuit.Solution.dynavars.h  := Value;
-         Set_IntervalHrs(Value);     // Keep IntervalHrs in synch with time step size
+         Set_IntervalHrs(Value/3600.0);     // Keep IntervalHrs in synch with time step size
      End;
 end;
 
