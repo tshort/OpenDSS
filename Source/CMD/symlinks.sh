ca46c0fe957781e85d8413744bed8a224e1d82ea
temcdrm <temcdrm@d8739450-1e93-4ef4-a0af-c327d92816ff>
2017-06-06 19:27:16 +0000

    helper file to enable linking libklusolve on Ubuntu
    git-svn-id: svn://svn.code.sf.net/p/electricdss/code/trunk@1929 d8739450-1e93-4ef4-a0af-c327d92816ff

---
+++ Source/CMD/symlinks.sh
@@ -0,0 +1,4 @@
+sudo ln -sv /usr/lib/x86_64-linux-gnu/libstdc++.so.6.0.21 /usr/lib/x86_64-linux-gnu/libstdc++.so
+sudo ln -sv /usr/lib/x86_64-linux-gnu/libstdc++.so.6 /usr/lib/x86_64-linux-gnu/libstdc++.so
+sudo ln -sv /lib/x86_64-linux-gnu/libgcc_s.so.1 /lib/x86_64-linux-gnu/libgcc_s.so
+
