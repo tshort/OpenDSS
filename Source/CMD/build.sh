19fe4c6d9c1edef7eda6bfe8559ecae6540a4aca
temcdrm <temcdrm@d8739450-1e93-4ef4-a0af-c327d92816ff>
2017-10-30 22:20:44 +0000

    building on Linux from the same build.sh
    git-svn-id: svn://svn.code.sf.net/p/electricdss/code/trunk@2056 d8739450-1e93-4ef4-a0af-c327d92816ff

--- Source/CMD/build.sh
+++ Source/CMD/build.sh
@@ -1,1 +1,6 @@
-ppcx64 @fpcopts.cfg -B opendsscmd.lpr
+OS="`uname`"
+if [[ "$OS" == "Linux" ]]; then
+	ppcx64 @linuxopts.cfg -B opendsscmd.lpr
+elif [[ "$OS" == "Darwin" ]]; then
+	ppcx64 @fpcopts.cfg -B opendsscmd.lpr
+fi
